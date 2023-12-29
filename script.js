3005155be78e8d2b871bf43339ceec427db5af9f
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2023-12-29 22:12:31 +0700

    Menambah script untuk fungsi search

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ toggleButton.addEventListener('click', function () {
     sidebar.classList.toggle('active');
     toggleButton.classList.toggle('active');
 });
+
+const formSearch=document.forms["formSearch"].querySelector("input");
+formSearch.addEventListener("keyup",function(e){
+    const input = e.target.value.toLowerCase();
+    const kumpulanProduk = document.querySelectorAll("#kontenProduk h1");
+    kumpulanProduk.forEach(function(i){
+        const namaProduk = i.innerText;
+        if(namaProduk.toLowerCase().indexOf(input)!=-1){
+           i.parentElement.parentElement.style.display="flex";
+        }
+        else{
+            i.parentElement.parentElement.style.display="none";
+        }
+    })
+})
